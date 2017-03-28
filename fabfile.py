92344029668993c4d7bf47ecfeb09897e95ca156
Nick Savchenko <nsavch@gmail.com>
2017-03-28 16:18:00 +0300

    Deployment stuff

--- fabfile.py
+++ fabfile.py
@@ -4,6 +4,7 @@ from fabric.api import *
 env.hosts = ['xon.teichisma.info']
 env.user = 'root'
 local_user = 'xanmel'
+local_home = '/home/xanmel/xanmel'
 app_path = '/home/xanmel/xanmel-web'
 venv_path = '/home/xanmel/venvs/xanmel-web'
 
@@ -15,11 +16,21 @@ def update_app():
 
 def install_requirements():
     with cd(app_path):
-        sudo('%s/bin/pip install -r requirements.txt' % venv_path, user=local_user)
+        sudo('HOME=%s %s/bin/pip install -r requirements.txt' % (local_home, venv_path), user=local_user)
+
+
+def update_systemd():
+    put('conf/xanmel-web.service', '/etc/systemd/system/')
+    run('systemctl daemon-reload')
+
+
+def update_nginx():
+    put('conf/nginx-site.conf', '/etc/nginx/sites-enabled/80xon.teichisma.info.conf')
+    run('systemctl restart nginx')
 
 
 def restart_server():
-    pass
+    run('systemctl restart xanmel-web')
 
 
 def deploy():
--- xanmel_web/config.py
+++ xanmel_web/config.py
@@ -5,3 +5,8 @@ class BaseConfig:
 class Dev(BaseConfig):
     DEBUG = True
     XANMEL_CONFIG = '../xanmel/xanmel.yaml'
+
+
+class Prod(BaseConfig):
+    DEBUG = False
+    XANMEL_CONFIG = '/etc/xanmel.yaml'
