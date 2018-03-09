928e3aacca3e5076d5e03e86e434ad9255711a13
Nick Savchenko <nsavch@gmail.com>
2018-03-09 23:16:21 +0200

    Adding link to the news feed page

--- xdf/urls.py
+++ xdf/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url('^$', main_page, name='main'),
     url('^rating/(\d+)/$', main_rating, name='rating'),
     url('^map/(\d+)/(\d+)/$', map_detail, name='map_detail'),
-    url('^compare/(\d+)/$', comparison, name='comparison')
+    url('^compare/(\d+)/$', comparison, name='comparison'),
+    url('^news-feed/(\d+)/$', news_feed, name='news_feed'),
 ]
