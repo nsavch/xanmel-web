a6fdaa6e7913c2ce5700cf059afefde44734f462
Nick Savchenko <nsavch@gmail.com>
2018-05-18 13:11:13 +0300

    Fix raven cfg

--- xanmel_web/settings/__init__.py
+++ xanmel_web/settings/__init__.py
@@ -60,7 +60,7 @@ if os.environ.get('SENTRY_DSN'):
         'dsn': os.environ['SENTRY_DSN'],
         # If you are using git, you can also automatically configure the
         # release based on the git info.
-        'release': raven.fetch_git_sha(os.path.abspath(os.pardir)),
+        'release': raven.fetch_git_sha(os.path.abspath('.')),
     }
 
 MIDDLEWARE = [
