f1d1617050333a14e1ec0331ab60c710355b266f
Nick Savchenko <nsavch@gmail.com>
2020-10-15 18:01:57 +0300

    Add django-dotenv

--- xanmel_web/settings/__init__.py
+++ xanmel_web/settings/__init__.py
@@ -15,7 +15,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import yaml
 from distutils.util import strtobool
+import dotenv
 
+dotenv.read_dotenv()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DEV = strtobool(os.environ.get('DEV', '0'))
